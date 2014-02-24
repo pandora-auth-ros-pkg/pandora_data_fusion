b674a01af8933844d7f6dff0f714fee2cb6c86f4
Triantafyllos Afouras <afourast@gmail.com>
2014-02-24 22:56:52 +0200

    added data_fusion package

---
+++ data_fusion/tests.cmake
@@ -0,0 +1,8 @@
+################# Tests ################################################
+#~ add_executable(pose_finder_test test/simulation_unit/pose_finder_test.cpp)
+#~ target_link_libraries(pose_finder_test 
+				    #~ ${catkin_LIBRARIES}
+				    #~ pose_finder
+				    #~ )
+
+				    
