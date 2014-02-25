9e8c39e1f9d5b69d0aa2544292edc080c6f65ede
Triantafyllos Afouras <afourast@gmail.com>
2014-02-25 04:14:25 +0200

    split data_fusion to pandora_alert_handler and pandora_victim_fusion, added launchers package and metapackage

--- data_fusion/tests.cmake
+++
@@ -1,8 +0,0 @@
-################# Tests ################################################
-#~ add_executable(pose_finder_test test/simulation_unit/pose_finder_test.cpp)
-#~ target_link_libraries(pose_finder_test 
-				    #~ ${catkin_LIBRARIES}
-				    #~ pose_finder
-				    #~ )
-
-				    
