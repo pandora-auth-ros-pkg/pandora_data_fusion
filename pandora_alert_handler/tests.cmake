89b5e7277c148b9b0bbbf572f106a7639b8dc75f
Konstantinos <chamzask@gmail.com>
2014-03-07 22:30:00 +0200

    Test for objects.cpp  named test_objects moved tests to directories and cmake for test and lint to separate file

--- pandora_alert_handler/tests.cmake
+++ pandora_alert_handler/tests.cmake
@@ -36,3 +36,18 @@ target_link_libraries(qr_test ${catkin_LIBRARIES} objects utils  qr gtest_main)
 
 catkin_add_gtest(hazmat_test test/hazmat_test.cpp)
 target_link_libraries(hazmat_test ${catkin_LIBRARIES} objects utils  hazmat gtest_main)
+
+catkin_add_gtest(objects_test test/unit/objects_test.cpp)
+target_link_libraries(objects_test ${catkin_LIBRARIES} 
+  objects 
+  utils 
+  gtest_main) 
+
+set(ROSLINT_CPP_OPTS 
+    "--filter=-whitespace/end_of_line,-build/include_order,-whitespace/blank_line,-whitespace/parens,-whitespace/comments")
+FILE(GLOB_RECURSE ${PROJECT_NAME}_LINT_SRCS 
+     RELATIVE ${PROJECT_SOURCE_DIR} 
+            include/alert_handler/*.h 
+            src/*.cpp 
+            )
+roslint_cpp(${${PROJECT_NAME}_LINT_SRCS})
