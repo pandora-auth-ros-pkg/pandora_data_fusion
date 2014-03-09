10cf151b57efb1143443b3375cef233cb7b1b019
Tsirigotis Christos <chritsir@auth.gr>
2014-03-09 19:02:46 +0200

    Broke down object.cpp to hazmat.cpp, hole.cpp, qr.cpp, tpa.cpp.

---
+++ pandora_alert_handler/src/hole.cpp
@@ -0,0 +1,41 @@
+// "Copyright [year] <Copyright Owner>"
+
+#include "alert_handler/objects.h"
+
+Hole::Hole() {
+  type_ = "hole";
+}
+
+geometry_msgs::PoseStamped Hole::getPoseStamped() const {
+
+  geometry_msgs::PoseStamped objPose = Object::getPoseStamped();
+  objPose.header.frame_id = "hole_" + boost::to_string(id_);
+  return objPose;
+}
+
+void Hole::getVisualization(visualization_msgs::MarkerArray* markers) const {
+
+  visualization_msgs::Marker marker;
+
+  marker.header.frame_id = "/world";
+  marker.header.stamp = ros::Time::now();
+  marker.ns = "Hole";
+  marker.id = id_;
+
+  marker.pose = pose_;
+
+  marker.type = visualization_msgs::Marker::SPHERE;
+
+  marker.scale.x = 0.1;
+  marker.scale.y = 0.1;
+  marker.scale.z = 0.1;
+
+  marker.color.r = 0;
+  marker.color.g = 0;
+  marker.color.b = 0;
+  marker.color.a = 0.7;
+
+  markers->markers.push_back(marker);
+
+}
+
---
+++ pandora_alert_handler/src/tpa.cpp
@@ -0,0 +1,41 @@
+// "Copyright [year] <Copyright Owner>"
+
+#include "alert_handler/objects.h"
+
+Tpa::Tpa() {
+  type_ = "tpa";
+}
+
+geometry_msgs::PoseStamped Tpa::getPoseStamped() const {
+
+  geometry_msgs::PoseStamped objPose = Object::getPoseStamped();
+  objPose.header.frame_id = "tpa_" + boost::to_string(id_);
+  return objPose;
+}
+
+void Tpa::getVisualization(visualization_msgs::MarkerArray* markers) const {
+
+  visualization_msgs::Marker marker;
+
+  marker.header.frame_id = "/world";
+  marker.header.stamp = ros::Time::now();
+  marker.ns = "Tpa";
+  marker.id = id_;
+
+  marker.pose = pose_;
+
+  marker.type = visualization_msgs::Marker::SPHERE;
+
+  marker.scale.x = 0.1;
+  marker.scale.y = 0.1;
+  marker.scale.z = 0.1;
+
+  marker.color.r = 0.545;
+  marker.color.g = 0.412;
+  marker.color.b = 0.08;
+  marker.color.a = 0.7;
+
+  markers->markers.push_back(marker);
+
+}
+
