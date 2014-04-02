d9575c240d0f53a6fdd88961810427691c3107d8
Tsirigotis Christos <chritsir@auth.gr>
2014-04-02 23:34:14 +0300

    Removes ROS_INFO output.

--- pandora_alert_handler/test/map_loader/src/map_loader.cpp
+++ pandora_alert_handler/test/map_loader/src/map_loader.cpp
@@ -103,17 +103,12 @@ namespace map_loader {
       exit(-1);
     }
     
-    ROS_INFO("Loading map from image \"%s\"", mapfname.c_str());
     map_server::loadMapFromFile(&map_resp_,mapfname.c_str(),
       res,negate,occ_th,free_th, origin);
       
     map_resp_.map.info.map_load_time = ros::Time::now();
     map_resp_.map.header.frame_id = frame_id;
     map_resp_.map.header.stamp = ros::Time::now();
-    ROS_INFO("Read a %d X %d map @ %.3lf m/cell",
-          map_resp_.map.info.width,
-          map_resp_.map.info.height,
-          map_resp_.map.info.resolution);
            
     return map_resp_.map;    
   } 
