b839b4bfaa8769ae9d42dba82d0c3de8654f51e3
Christos Tsirigotis <tsirif@gmail.com>
2015-06-20 03:31:01 +0300

    Changes round to floor in COORDS macro in defines.h

--- pandora_alert_handler/include/alert_handler/defines.h
+++ pandora_alert_handler/include/alert_handler/defines.h
@@ -58,8 +58,8 @@
 #define DEGREE (PI / 180.0)
 
 //!< Macro to convert to map coordinates from meters.
-#define COORDS(X, Y, MAP) static_cast<int>(round((X - MAP->info.origin.position.x)\
-    / MAP->info.resolution) + round((Y - MAP->info.origin.position.y)\
+#define COORDS(X, Y, MAP) static_cast<int>(floor((X - MAP->info.origin.position.x)\
+    / MAP->info.resolution) + floor((Y - MAP->info.origin.position.y)\
       / MAP->info.resolution) * MAP->info.width)
 
 //!< Macro to convert to map coordinates from meters.
