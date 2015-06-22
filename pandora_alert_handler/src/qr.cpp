4b1a0637c496e3ba74bd58c6e04d421a85d85290
Christos Tsirigotis <tsirif@gmail.com>
2015-06-22 20:56:13 +0300

    Qr is found in List according to its content, override isSameObject

--- pandora_alert_handler/src/qr.cpp
+++ pandora_alert_handler/src/qr.cpp
@@ -40,55 +40,51 @@
 
 namespace pandora_data_fusion
 {
-  namespace pandora_alert_handler
+namespace pandora_alert_handler
+{
+  Qr::Qr() {}
+
+  bool Qr::isSameObject(const ObjectConstPtr& object) const
   {
-    Qr::Qr() {}
+    if (this->getType() != object->getType())
+      return false;
+    return !content_.compare(boost::dynamic_pointer_cast<Qr const>(object)->getContent());
+  }
 
-    bool Qr::isSameObject(const ObjectConstPtr& object) const
-    {
-      bool cond = Object<Qr>::isSameObject(object)
-        && !content_.compare(
-            boost::dynamic_pointer_cast<const Qr>(object)->getContent());
+  void Qr::getVisualization(visualization_msgs::MarkerArray* markers) const
+  {
+    visualization_msgs::Marker marker;
+    marker.header.frame_id = Qr::getGlobalFrame();
+    marker.header.stamp = ros::Time::now();
+    marker.ns = type_;
+    marker.id = id_;
+    marker.pose = pose_;
+    marker.type = visualization_msgs::Marker::SPHERE;
+    marker.scale.x = 0.1;
+    marker.scale.y = 0.1;
+    marker.scale.z = 0.1;
+    marker.color.r = 0;
+    marker.color.g = 0;
+    marker.color.b = 1;
+    marker.color.a = 0.7;
+    markers->markers.push_back(marker);
 
-      return cond;
-    }
-
-
-    void Qr::getVisualization(visualization_msgs::
-        MarkerArray* markers) const
-    {
-      visualization_msgs::Marker marker;
-      marker.header.frame_id = Qr::getGlobalFrame();
-      marker.header.stamp = ros::Time::now();
-      marker.ns = type_;
-      marker.id = id_;
-      marker.pose = pose_;
-      marker.type = visualization_msgs::Marker::SPHERE;
-      marker.scale.x = 0.1;
-      marker.scale.y = 0.1;
-      marker.scale.z = 0.1;
-      marker.color.r = 0;
-      marker.color.g = 0;
-      marker.color.b = 1;
-      marker.color.a = 0.7;
-      markers->markers.push_back(marker);
-
-      visualization_msgs::Marker description;
-      description.header.frame_id = Qr::getGlobalFrame();
-      description.header.stamp = ros::Time::now();
-      description.ns = type_ + "_BRIEF";
-      description.id = id_;
-      description.pose = pose_;
-      description.pose.position.z = pose_.position.z + 0.1;
-      description.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
-      description.text = getFrameId() + "_" + content_;
-      description.scale.z = 0.1;
-      description.color.r = 0;
-      description.color.g = 0;
-      description.color.b = 1;
-      description.color.a = 0.7;
-      markers->markers.push_back(description);
-    }
+    visualization_msgs::Marker description;
+    description.header.frame_id = Qr::getGlobalFrame();
+    description.header.stamp = ros::Time::now();
+    description.ns = type_ + "_BRIEF";
+    description.id = id_;
+    description.pose = pose_;
+    description.pose.position.z = pose_.position.z + 0.1;
+    description.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
+    description.text = getFrameId() + "_" + content_;
+    description.scale.z = 0.1;
+    description.color.r = 0;
+    description.color.g = 0;
+    description.color.b = 1;
+    description.color.a = 0.7;
+    markers->markers.push_back(description);
+  }
 
 }  // namespace pandora_alert_handler
 }  // namespace pandora_data_fusion
